98451bb7640ecabea7e5d4a719f26aee9f226a04
Leandro Jr <leandrojr70@gmail.com>
2021-04-07 15:20:37 -0300

    puxando frases logicamente

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -14,6 +15,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  var _frases= [
+    "Frase1",
+    "Frase2",
+    "Frase3",
+    "Frase4",
+  ];
+  var _fraseGerada = "Clique abaixo para gerar uma frase!";
+
+  void _gerarFrase(){
+    var numeroSorteado = Random().nextInt(_frases.length);
+
+    setState(() {
+      _fraseGerada = _frases[numeroSorteado];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +49,7 @@ class _HomeState extends State<Home> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               Image.asset("images/logo.png"),
-              Text("Clique abaixo para gerar uma frase!",
+              Text(_fraseGerada,
                 textAlign: TextAlign.justify,
                 style: TextStyle(
                   fontSize: 25,
@@ -49,7 +66,7 @@ class _HomeState extends State<Home> {
                   ),
                 ),
                 color: Colors.green,
-                onPressed: (){},
+                onPressed: _gerarFrase,
               )
             ],
           ),
